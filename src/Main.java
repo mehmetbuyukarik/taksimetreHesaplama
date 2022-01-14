ddae3151eb80f9e147fc2801ce272b3478914ed6
HP 8460P <mbuyukarik@gmail.com>
2022-01-14 21:20:27 +0300

    hipotenus Hesaplama

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[]args){
+
+        int a, b ;
+        double c ;
+
+
+        Scanner girdi = new Scanner(System.in);
+        System.out.print("Birinci  Kenarı giriniz :  ");
+        a = girdi.nextInt();
+
+        System.out.print("İkinci Kenarı giriniz :  ");
+
+        b = girdi.nextInt() ;
+
+        c = Math.sqrt((a*a) + (b*b) ) ;
+        System.out.println( "Hipotenüs  : " +c );
+
+    }
+
+
+}
