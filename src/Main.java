f12d857c1d3c04129cd53983325dc3472294eb73
HP 8460P <mbuyukarik@gmail.com>
2022-01-14 21:44:49 +0300

    hipotenus Hesaplama

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,20 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main (String[]args){
-
-        int a, b ;
-        double c ;
-
+    public static void main(String[] args) {
+        int km;
+        double perKm = 2.20, total = 10, startPrice = 10;
 
         Scanner girdi = new Scanner(System.in);
-        System.out.print("Birinci  Kenarı giriniz :  ");
-        a = girdi.nextInt();
+        System.out.println("Mesafeyi km cinsinden giriniz : ");
+        km = girdi.nextInt();
 
-        System.out.print("İkinci Kenarı giriniz :  ");
 
-        b = girdi.nextInt() ;
+        total = (km * perKm);
+        total += startPrice;
+        total = (total < 20) ? 20 : total;
+        System.out.println("Toplam Tutar :" +total);
 
-        c = Math.sqrt((a*a) + (b*b) ) ;
-        System.out.println( "Hipotenüs  : " +c );
 
     }
-
-
 }
